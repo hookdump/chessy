30ed5a8d5fb9c1710ce969473bbc2d0406c497c3
Ignacio Freiberg <hookdump@gmail.com>
2020-01-23 14:28:38 -0300

    Allow to un/block squares with right click

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
-  
+    $(this).bind("contextmenu", function(e) {
+        e.preventDefault();
+    });
+
   var board = [];
   var tagged = {};
+  var blocked = {}
 
   var index = {};
   var rows = ['8','7','6','5','4','3','2','1'];
   var files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-  
+  currentPos = null;
 
   var initBoard = () => {
     board = [];
@@ -17,7 +21,6 @@ $(document).ready(function() {
     var addRow = (rowId) => {
       var row = $("<div class='row'></div>");
       board[rowId] = [];
-      tagged[rowId] = [];
 
       for (var i=0; i<8; i++) {
         var identifier = files[i] + rows[rowId];
@@ -30,13 +33,13 @@ $(document).ready(function() {
         board[rowId][i] = identifier;
         index[identifier] = [rowId, i];
       }
-      $('.board').append(row);  
+      $('.board').append(row);
     }
-    
+
     for (var i=0; i<8; i++) {
       addRow(i);
     }
-    $(".square").on("click", clickHandler);
+    $(".square").on("mousedown", clickHandler);
   };
 
   var initPiece = (type, square) => {
@@ -45,12 +48,14 @@ $(document).ready(function() {
     $("." + square).find(".txt").html("&nbsp;");
   };
 
-  var tagged = [];
-
   var tagSquare = (x, y, val, steps) => {
     var sq = null;
     if (x >= 0 && x < 8 && y >= 0 && y < 8) {
       sq = board[x][y];
+      if (blocked[sq]) {
+        console.log("Square " + sq + " is blocked!");
+        return {};
+      }
       if (!tagged[sq] || tagged[sq] > val) {
         var cur = $("." + sq).find(".txt").text();
         $("." + sq).find(".txt").text(val);
@@ -60,7 +65,6 @@ $(document).ready(function() {
           return {x: x, y: y, val: ++val, steps: --steps};
         }
       }
-      
     }
   }
 
@@ -84,19 +88,36 @@ $(document).ready(function() {
 
   function refresh(sq) {
     console.log('REFRESH', sq);
+    currentPos = sq;
     initBoard();
     initPiece('N', sq);
     var pos = index[sq];
     var x = pos[0];
-    var y = pos[1]; 
+    var y = pos[1];
     renderPath(x, y, 1, 5);
     $("." + sq).find(".txt").addClass("hide").html("&nbsp;");
+    console.log(tagged);
+  }
+
+  function toggleBlock(sq) {
+    if (blocked[sq]) {
+      blocked[sq] = false;
+    } else {
+      blocked[sq] = true;
+    }
+    console.log("BLOCKED", blocked);
+    refresh(currentPos);
   }
 
   function clickHandler(event) {
     var target = $(event.currentTarget);
     var id = target.find(".identifier").text();
-    refresh(id);
+    if (event.which == 1) {
+      refresh(id);
+    } else {
+      event.preventDefault();
+      toggleBlock(id);
+    }
   }
 
   refresh('g1');
